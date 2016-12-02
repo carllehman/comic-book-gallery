d389acb854c67b56b5d218ffcb6167ccc79d03ca
clehman <clehman@aafp.org>
2016-12-02 09:55:18 -0600

    Updated Comic book controller to use repository

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -3,28 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ComicBookGallery.Data;
 using ComicBookGallery.Models;
 
 namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
-        public ActionResult Detail()
+
+        private ComicBookRepository _comicBookRepository = null;
+
+        public ComicBooksController()
         {
-            var comicBook = new ComicBook()
+            _comicBookRepository = new ComicBookRepository();
+        }
+
+
+        public ActionResult Detail(int? id)
+        {
+            if (id == null)
             {
-                SeriesTitle = "The Amazing Spider-Man"
-                , IssueNumber = 700
-                , DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>"
-                , Artists = new Artist[]
-                {
-                    new Artist() { Role = "Script", Name="Dan Slott"}
-                    , new Artist() { Role = "Pencils", Name="Humberto Ramos"}
-                    , new Artist() { Role = "Inks", Name="Victor Olazaba"}
-                    , new Artist() { Role = "Colors", Name="Edgar Delgado"}
-                    , new Artist() { Role = "Letters", Name="Chris Eliopoulos"}
-                }
-            };
+                return HttpNotFound();
+            }
+            var comicBook = _comicBookRepository.GetComicBook(id.Value);
             return View(comicBook);
         }
     }
